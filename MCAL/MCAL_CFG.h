28d594cb23c92836f423104d710f7f2ecbdf56ba
jinxinyang <jinxinyang@chengtech.com>
2023-09-22 11:28:46 +0800

    添加基本框架的头文件

--- MCAL/MCAL_CFG.h
+++ MCAL/MCAL_CFG.h
@@ -4,8 +4,23 @@
 
 /*Chip Platform Definition*/
 #define X86 0
-
+#define STM32F10X 1
+#define ESP32 2
 #define PLATFORM_CONFIG X86
 
+#if PLATFORM_CONFIG == X86
+#include "x86/MCAL_Core_CFG.h"
+#include "x86/MCAL_CAN_CFG.h"
+#include "x86/MCAL_UART_CFG.h"
+#include "x86/MCAL_EEPROM_CFG.h"
+#include "x86/MCAL_Flash_CFG.h"
+#include "x86/MCAL_Etherent_CFG.h"
+#elif PLATFORM_CONFIG == STM32F10X
+
+#elif PLATFORM_CONFIG == ESP32
+
+#else
+#error "Please define the platform you are using"
+#endif
 
 #endif/*MCAL_CFG_H*/
