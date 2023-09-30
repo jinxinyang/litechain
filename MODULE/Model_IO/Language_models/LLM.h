d5b4e3bacada1b2d597b557c0d37d7b4afefdf5e
jinxinyang <jinxinyang@chengtech.com>
2023-09-30 14:11:32 +0800

    增加LLM的头文件

---
+++ MODULE/Model_IO/Language_models/LLM.h
@@ -0,0 +1,15 @@
+/*
+ * LLM.h
+ *
+ *  Created on: 2023年9月28日
+ *      Author: jinxinyang
+ */
+
+#ifndef LITECHAIN_MODULE_MODEL_IO_LANGUAGE_MODELS_LLM_H_
+#define LITECHAIN_MODULE_MODEL_IO_LANGUAGE_MODELS_LLM_H_
+
+
+#include "LLM_Openai.h"
+
+
+#endif /* LITECHAIN_MODULE_MODEL_IO_LANGUAGE_MODELS_LLM_H_ */
