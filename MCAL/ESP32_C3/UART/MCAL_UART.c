e7d482265e6fbf72c62628b1a5f18a4e1c313141
jinxinyang <jinxinyang@chengtech.com>
2023-10-17 09:18:54 +0800

    增加ESP32的MCAL串口驱动

--- MCAL/ESP32_C3/UART/MCAL_UART.c
+++ MCAL/ESP32_C3/UART/MCAL_UART.c
@@ -1,26 +1,55 @@
 /*
     * MCAL_UART.c
-    *
+    *  esp32的串口通信封装
+    *  
     *  Created on: 2023.9.22
 */
 
 #include "MCAL_UART.h"
+#include "driver/gpio.h"
+#include "driver/uart.h"
+#include "string.h"
+
+
+
 
 int UART_Init(int baud);
 int UART_SendString(char *str,int len);
-int UART_ReceiveString(char *str);
+int UART_ReceiveString(char *str, int max_len);
 
+//串口初始化
 int UART_Init(int baud)
 {
-	return 0;
+    const uart_config_t uart_config = {
+        .baud_rate = baud,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_DEFAULT,
+    };
+    // We won't use a buffer for sending data.
+    uart_driver_install(UART_NUM_1, RX_BUF_SIZE * 2, 0, 0, NULL, 0);
+    uart_param_config(UART_NUM_1, &uart_config);
+    uart_set_pin(UART_NUM_1, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+    return 0;
 }
-
+//串口发送字符串
 int UART_SendString(char *str,int len)
 {
-	return 0;
+    const int txBytes = uart_write_bytes(UART_NUM_1, str, len);
+    return txBytes;
 }
 
-int UART_ReceiveString(char *str)
+//串口接收字符串
+int UART_ReceiveString(char *str, int max_len)
 {
-	return 0;
+    uint8_t* data = (uint8_t*) malloc(max_len+1);
+    const int rxBytes = uart_read_bytes(UART_NUM_1, data, max_len, 1000 / portTICK_PERIOD_MS);
+    if (rxBytes > 0) {
+        data[rxBytes] = 0;
+        memcpy(str,data,rxBytes);
+    }
+    free(data);
+    return rxBytes;
 }
--- MCAL/ESP32_C3/UART/MCAL_UART.h
+++ MCAL/ESP32_C3/UART/MCAL_UART.h
@@ -3,9 +3,14 @@
 
 #include "../../MCAL_CFG.h"
 
+#define RX_BUF_SIZE 256
+#define TXD_PIN 17
+#define RXD_PIN 16
+
+
 extern int UART_Init(int baud);
 extern int UART_SendString(char *str,int len);
-extern int UART_ReceiveString(char *str);
+extern int UART_ReceiveString(char *str, int max_len);
 
 
 #endif /* MCAL_UART_H */
